c517a31ddeac8a3b7c57711d77966831f38667d3
Brenda-G06 <135345282+Brenda-G06@users.noreply.github.com>
2024-07-01 09:24:57 -0300

    2 commit

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-angular.module('blogLunar', []);
+angular.module("blogLunar", []);
 
-angular.module('blogLunar').controller('Rest', function($scope, $http) {
-    $http.get('https://api-fake-blog.onrender.com/postagens')
-        .success(function(data) {
-            $scope.publicacoes = data;
-        })
-        .error(function() {
-            console.log("Erro ao carregar as postagens.");
-        });
+angular.module("blogLunar").controller("Rest", function ($scope, $http) {
+  $http.get("https://api-fake-blog.onrender.com/postagens")
+    .then(function (response) {
+      $scope.publicacoes = response.data;
+    });
 });
 
+angular.module("blogLunar").controller("DetailsController", function ($scope, $http, $location) {
+  var params = new URLSearchParams(window.location.search);
+  var id = params.get('id');
+
+  if (id) {
+    $http.get("https://api-fake-blog.onrender.com/postagem/" + id)
+      .then(function (response) {
+        $scope.publicacao = response.data;
+      });
+  } else {
+    console.error("Parâmetro id não encontrado na URL");
+  }
+});
